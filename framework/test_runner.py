ff7597789e4f78b105a4ed425e5bd5ee8851db3a
carsonapple <carsonapple2029@u.northwestern.edu>
2026-07-06 18:12:34 -0700

    small changes

--- framework/test_runner.py
+++ framework/test_runner.py
@@ -1,16 +1,24 @@
 import subprocess
 from framework.logger import get_logger
+from pathlib import Path
 
 logger = get_logger(__name__)
+
 
 def run_tests():
     logger.info("Starting test run")
 
+    reports_dir = Path("reports")
+    reports_dir.mkdir(exist_ok=True)
+
     result = subprocess.run(
         [
+            "python",
+            "-m",
             "pytest",
             "tests",
-            "--html=reports/report.html"
+            "--html=reports/report.html",
+            "--self-contained-html",
         ],
         capture_output=True,
         text=True
@@ -18,9 +26,14 @@ def run_tests():
 
     with open(
         "reports/latest_test_run.txt",
-        "w"
+        "w",
+        encoding="utf-8"
     ) as report:
+        report.write("STDOUT\n")
         report.write(result.stdout)
+
+        report.write("\n\nSTDERR\n") #two blank lines between stdout and stderr
+        report.write(result.stderr)
 
     if result.returncode == 0:
         logger.info("All tests passed")
